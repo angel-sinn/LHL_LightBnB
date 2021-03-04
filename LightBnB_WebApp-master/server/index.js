2bab6a7ffd7132ac80c30977da99a8e039fd8ec2
Angel Sinn <angelkysinn@gmail.com>
2021-03-04 22:57:34 +0000

    Refactored code to include index.js file to follow suggested project structure

---
+++ LightBnB_WebApp-master/server/index.js
@@ -0,0 +1,15 @@
+// Connect to database using node-postgres
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "lightbnb",
+});
+
+module.exports = {
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+};
